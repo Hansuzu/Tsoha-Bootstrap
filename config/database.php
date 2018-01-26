3f9c4d2cdf5b8bafa3f026601784149e06fd9cab
Hansuzu <hannes.ihalainen@gmail.com>
2018-01-26 20:50:09 +0200

    e

--- config/database.php
+++ config/database.php
@@ -16,9 +16,9 @@
         'resource' => 'pgsql:'
       ),
       'mysql' => array(
-        'resource' => 'mysql:unix_socket=/home/KAYTTAJATUNNUS/mysql/socket;dbname=mysql',
+        'resource' => 'mysql:unix_socket=/home/haih/mysql/socket;dbname=mysql',
         'username' => 'root',
-        'password' => 'SALASANA'
+        'password' => '1234'
       )
     );
 
