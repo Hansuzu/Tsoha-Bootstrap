4b6dd56355e46d216dff3f99ab6d4412237e4187
Hansuzu <hannes.ihalainen@gmail.com>
2018-02-11 16:48:36 +0200

    e

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -6,7 +6,7 @@ INSERT INTO AbstractArticle DEFAULT VALUES;
 INSERT INTO Language (name) VALUES ('Abbagabba');
 INSERT INTO Article (abstract_id,language_id,readonly,name) VALUES (1,1,b'1','diipadaapa');
 INSERT INTO Article (abstract_id,language_id,readonly,name) VALUES (2,1,b'1','diipadaapa2');
-INSERT INTO ArticleVersion (article_id,parent_id,user_id,time,active,contents) VALUES (1,NULL,1,NOW(),1,'tietoa');
+INSERT INTO ArticleVersion (article_id,parent_id,user_id,time,active,contents) VALUES (1,NULL,1,NOW(),b'1','tietoa');
 INSERT INTO ArticleSuperClass (suparticle_id,subarticle_id) VALUES (1,2);
 INSERT INTO Message (article_id,user_id,message,time,edited) VALUES (1,1,'viestini',NOW(),NOW());
 
